add3c52964924b3ef08df6b02586b950ae1de7a1
nordl1ng <107497909+nordl1ng@users.noreply.github.com>
2022-09-13 13:19:16 +0500

    2 готова

--- task2.py
+++ task2.py
@@ -2,3 +2,16 @@
 # Пример:
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+
+listt=input(("Введите элементы массива через пробел: ")).split(" ")
+list_int=[int(item) for item in listt]
+
+def couple(list):
+    list_couple = []
+    a = int(((len(list)-1)/2))
+    for i in range(0,a+1):
+        list_couple.append(list[i]*list_int[len(list)-1-i])
+    print(f"Произведение элементов: {list_couple}")
+
+
+couple(list_int)
