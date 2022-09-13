2c984cdbfba1c20e69c58c9368edb93fc70de789
nordl1ng <107497909+nordl1ng@users.noreply.github.com>
2022-09-13 16:44:12 +0500

    два знака

--- task3.py
+++ task3.py
@@ -6,15 +6,18 @@
 listt=input(("Введите элементы массива через пробел: ")).split(" ")
 list_float=[float(item) for item in listt]
 
-import math
-a, b = math.modf(list_float[0])
-max=a
-min=a
-for i in range(1,len(list_float)):
-    a, b = math.modf(list_float[i])
-    if a > max: 
-        max = a
-    if a < min:
-        min = a
-h=float(max-min)
-print(h)
+def minmax(list):
+    import math
+    a, b = math.modf(list[0])
+    max=a
+    min=a
+    for i in range(1,len(list)):
+        a, b = math.modf(list[i])
+        if a > max: 
+            max = a
+        if a < min:
+            min = a
+    v=str(max-min)
+    print(v[:4])
+
+minmax(list_float)
